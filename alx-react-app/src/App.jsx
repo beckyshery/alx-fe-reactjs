273d992d5fdcb1be776d0f69783fe262bf6fcd5e
oyoo <rebeccaadhiambooyoo006@gmail.com>
2024-11-10 12:59:26 -0800

    reactjs

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,8 +1,8 @@
 import WelcomeMessage from './components/WelcomeMessage'
 import { useState } from 'react'
-import Header from './components/Header';
-import Footer from './components/Footer
-import maincontent'./ from './components/MainContent';
+import Header from './components/header;
+import Footer from './components/Footer'
+import MainContent './components/MainContent;
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
