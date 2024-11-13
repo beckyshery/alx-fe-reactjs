8faf458e8c09a32ecac81f4267277f4ec7b07526
oyoo <rebeccaadhiambooyoo006@gmail.com>
2024-11-12 22:03:21 -0800

    react app new

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 function MainContent() {
   return (
-    <main>
-      <p>I love to visit New York, Paris, and Tokyo.</p>
+    <main style={{ padding: '20px', backgroundColor: '#f4f4f4' }}>
+      <h2 style={{ color: 'darkslategray' }}>Main Content</h2>
+      <p>This is where the main content will go.</p>
     </main>
   );
 }
