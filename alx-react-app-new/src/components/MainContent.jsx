f8ef593822a88b26ee38546423da9f8cc42033e6
oyoo <rebeccaadhiambooyoo006@gmail.com>
2024-11-12 22:13:44 -0800

    react app new

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -4,7 +4,7 @@ function MainContent() {
   return (
     <main style={{ padding: '20px', backgroundColor: '#f4f4f4' }}>
       <h2 style={{ color: 'darkslategray' }}>Main Content</h2>
-      <p>i love travelling the world.</p>
+      <p>"I love to visit New York, Paris, and Tokyo."</p>
     </main>
   );
 }
