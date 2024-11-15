e31e05196e3da23cbde9237749877877e456a9f2
oyoo <rebeccaadhiambooyoo006@gmail.com>
2024-11-14 23:11:52 -0800

    Ensure UserContext.js and other updates are added and tracked

--- alx-react-app-props/src/components/UserDetails.jsx
+++ alx-react-app-props/src/components/UserDetails.jsx
@@ -1,7 +1,26 @@
 import React, { useContext } from 'react';
 import UserContext from '../UserContext'; 
 
-function UserDetails({userData}) {
+function UserDetails({userDataimport React, { useContext } from 'react';
+import UserContext from '../UserContext';console.log('UserDetails component rendered');
+console.log('User data:', userData);
+
+function UserDetails() {
+  const userData = useContext(UserContext);
+
+  if (!userData) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
+}
+
+export default UserDetails;}) {
   const userData = useContext(UserContext); 
 
   return (
