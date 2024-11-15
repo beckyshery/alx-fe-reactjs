7dea9ef670ee09768780a16e95b7f36c5af9570a
oyoo <rebeccaadhiambooyoo006@gmail.com>
2024-11-15 00:22:23 -0800

    react app props

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,9 +1,30 @@
-// src/components/UserInfo.jsx
-import React from 'react';
-import UserDetails from './UserDetails';
+import React, { useContext } from 'react';
+import { UserContext } from '../components/UserContext'; 
 
-function UserInfo() {
-  return <UserDetails />; // No need to pass userData as prop anymore
-}
+const UserProfile = () => {
+  const { user, loading, error } = useContext(UserContext);
 
-export default UserInfo;
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <div>
+      <h1>User Profile</h1>
+      {user ? (
+        <div>
+          <p>Name: {user.name}</p>
+          <p>Email: {user.email}</p>
+        </div>
+      ) : (
+        <p>No user data available.</p>
+      )}
+    </div>
+  );
+};
+
+export default UserProfile;
