b65dedccc0f0d3bcbcf05e3edb7339868865294e
oyoo <rebeccaadhiambooyoo006@gmail.com>
2024-11-15 11:44:59 -0800

    recipe app

--- Desktop/recipe-sharing-app/recipe-sharing-app/src/App.jsx
+++ Desktop/recipe-sharing-app/recipe-sharing-app/src/App.jsx
@@ -2,8 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import RecipeList from '/recipelistcomponent'
-import AddRecipeForm from '/addrecipeform component'
+import recipelist from '/components/recipelist'
+import addrecipeform from '/components/addrecipeform'
+
 function App() {
   const [count, setCount] = useState(0)
 
--- Desktop/recipe-sharing-app/recipe-sharing-app/src/components/AddRecipeForm
+++ Desktop/recipe-sharing-app/recipe-sharing-app/src/components/AddRecipeForm
@@ -1,4 +1,4 @@
- import { useState } from 'react';
+import { useState } from 'react';
   import { useRecipeStore } from './recipeStore';
 
   const AddRecipeForm = () => {
