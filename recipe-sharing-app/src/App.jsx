d6d5f3fbdc48af69b864e2b1ea10d7ea5eb02cc1
oyoo <rebeccaadhiambooyoo006@gmail.com>
2024-11-16 23:28:50 -0800

    recipe sharing app

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,19 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import AddRecipeForm from './components/AddRecipeForm'
-import RecipeList from './components/RecipeList'
+import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import necessary components
+import './App.css';
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeList from './components/RecipeList';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-        <RecipeList />
-        <AddRecipeForm />
-    </>
-  )
+    <Router> {/* Wrap your app with Router */}
+      <div>
+        <h1>Recipe App</h1>
+        <Routes>
+          <Route path="/" element={<RecipeList />} /> {/* Route for RecipeList */}
+          <Route path="/add" element={<AddRecipeForm />} /> {/* Route for AddRecipeForm */}
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
