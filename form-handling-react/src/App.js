f80a0aa2a1e93f3ab3f394e0134d5f7641a00e86
oyoo <rebeccaadhiambooyoo006@gmail.com>
2024-11-19 04:24:44 -0800

    form handling react

---
+++ form-handling-react/src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import RegistrationForm from './components/RegistrationForm';
+
+function App() {
+  return (
+    <div>
+      <h1>User Registration</h1>
+      <RegistrationForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ form-handling-react/src/components/formikForm.js
@@ -0,0 +1,13 @@
+// src/components/FormikForm.js
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+const validationSchema = Yup.object({
+  username: Yup.string().required('Username is required'),
+  email: Yup.string().email('Invalid email format').required('Email is required'),
+  password: Yup.string().required('Password is required'),
+});
+
+const FormikForm = () => {
+ 
