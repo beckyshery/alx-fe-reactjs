017272df3acbf12a08d168983152310ebb2d6086
oyoo <rebeccaadhiambooyoo006@gmail.com>
2024-11-19 04:38:39 -0800

    form handling react

--- form-handling-react/src/App.js
+++ form-handling-react/src/App.js
@@ -1,12 +1,16 @@
 // src/App.js
 import React from 'react';
 import RegistrationForm from './components/RegistrationForm';
+import FormikForm from './components/FormikForm';
 
 function App() {
   return (
     <div>
       <h1>User Registration</h1>
+      <h2>Controlled Form</h2>
       <RegistrationForm />
+      <h2>Formik Form</h2>
+      <FormikForm />
     </div>
   );
 }
--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -10,4 +10,38 @@ const validationSchema = Yup.object({
 });
 
 const FormikForm = () => {
- 
+  return (
+    <Formik
+      initialValues={{ username: '', email: '', password: '' }}
+      validationSchema={validationSchema}
+      onSubmit={(values, { resetForm }) => {
+        console.log('Form submitted:', values);
+        // Here you would typically send a request to your API
+        resetForm();
+      }}
+    >
+      {() => (
+        <Form>
+          <div>
+            <label>Username:</label>
+            <Field type="text" name="username" />
+            <ErrorMessage name="username" component="div" />
+          </div>
+          <div>
+            <label>Email:</label>
+            <Field type="email" name="email" />
+            <ErrorMessage name="email" component ="div" />
+          </div>
+          <div>
+            <label>Password:</label>
+            <Field type="password" name="password" />
+            <ErrorMessage name="password" component="div" />
+          </div>
+          <button type="submit">Register</button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+export default FormikForm;
