cf3d7322c585feb10a793f561ecce738dba8e183
oyoo <rebeccaadhiambooyoo006@gmail.com>
2024-11-28 06:03:49 -0800

    tailwind react integration

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,10 +1,16 @@
+
+import React from "react";
+import UserProfile from "./components/UserProfile";
 import './App.css'
 
 function App() {
 
   return (
+    
     <div className="flex items-center justify-center min-h-screen bg-blue-500">
       <h1 className="text-4xl font-bold text-white">Welcome to Tailwind CSS with React!</h1>
+      className="App"
+      <UserProfile />
     </div>
   )
 }
