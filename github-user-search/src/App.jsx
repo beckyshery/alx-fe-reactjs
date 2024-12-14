bb42cc314351934251840d9cd24b1e5a60c5b851
oyoo <rebeccaadhiambooyoo006@gmail.com>
2024-12-14 04:57:53 -0800

    git hubuser research

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>GitHub User Search</h1>
+      {/* Add your components here */}
+    </div>
+  );
+}
+
+export default App;
