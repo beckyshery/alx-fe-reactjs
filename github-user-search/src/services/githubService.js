31980ce7286f0d3dbd3243b95d9fb321ca7a96aa
oyoo <rebeccaadhiambooyoo006@gmail.com>
2024-12-14 08:18:54 -0800

    gihub user research

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const GITHUB_API_URL = 'https://api.github.com/users/';
 
-export const fetchUser Data = async (username) => {
+export const fetchUserData = async (username) => {
     const response = await axios.get(`${GITHUB_API_URL}${username}`);
     return response.data;
 };
